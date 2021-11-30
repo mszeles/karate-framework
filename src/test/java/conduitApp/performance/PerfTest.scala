11d3f35d79f61df30e13bb5b66be647084fd91a4
Miklos Szeles <miklos.szeles@gmail.com>
2021-11-30 15:27:38 +0100

    Simulation setup for open model

--- src/test/java/conduitApp/performance/PerfTest.scala
+++ src/test/java/conduitApp/performance/PerfTest.scala
@@ -18,7 +18,21 @@ class PerfTest extends Simulation {
 
     setUp(
         create.inject(
-            atOnceUsers(5)
+            atOnceUsers(1),
+            nothingFor(4 seconds),
+            constantUsersPerSec(1) during (10 seconds),
+            constantUsersPerSec(2) during (10 seconds),
+            rampUsersPerSec(2) to 10 during (20 seconds),
+            nothingFor(5 seconds),
+            constantUsersPerSec(1) during (5 seconds)
+            // nothingFor(2 seconds),  //Pause for a given duration.
+            // atOnceUsers(5), //Injects a given number of users at once.
+            // rampUsers(10) during (5 seconds), //Injects a given number of users distributed evenly on a time window of a given duration.
+            // constantUsersPerSec(20) during (15 seconds), //Injects users at a constant rate, defined in users per second, during a given duration. Users will be injected at regular intervals.
+            // constantUsersPerSec(20) during (15 seconds) randomized, //Injects users at a constant rate, defined in users per second, during a given duration. Users will be injected at randomized intervals.
+            // rampUsersPerSec(10) to 20 during (10 minutes),  //Injects users from starting rate to target rate, defined in users per second, during a given duration. Users will be injected at regular intervals.
+            // rampUsersPerSec(10) to 20 during (10 minutes) randomized, //Injects users from starting rate to target rate, defined in users per second, during a given duration. Users will be injected at randomized intervals.
+
         ).protocols(protocol),
     )
 
