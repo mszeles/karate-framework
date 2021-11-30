a5f5c9b564cd66734d859c53107273f6b0b4c80a
Miklos Szeles <miklos.szeles@gmail.com>
2021-11-30 10:17:54 +0100

    Added cucumber reporting

--- src/test/java/conduitApp/ConduitTest.java
+++ src/test/java/conduitApp/ConduitTest.java
@@ -2,12 +2,21 @@ package conduitApp;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
 import com.intuit.karate.junit5.Karate;
 //import com.intuit.karate.junit5.Karate.Test;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Test;
+
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
 
 //@KarateOptions(tags = {"@debug"})
 class ConduitTest {
@@ -22,7 +31,17 @@ class ConduitTest {
         Results results = Runner.path("classpath:conduitApp")
                 .outputCucumberJson(true)
                 .parallel(5);
+        generateReport(results.getReportDir());
         assertEquals(0, results.getFailCount(), results.getErrorMessages());
+    }
+
+    public static void generateReport(String karateOutputPath) {
+        Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[] {"json"}, true);
+        List<String> jsonPaths = new ArrayList<>(jsonFiles.size());
+        jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
+        Configuration config = new Configuration(new File("target"), "conduitApp");
+        ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
+        reportBuilder.generateReports();
     }
 
 }
